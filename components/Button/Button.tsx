a788764be343c32c93e8f3eb109e3ecde9f53805
Igor Iugin <animeneanime@mail.ru>
2021-12-12 16:53:53 +0300

    Add main structure for button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,16 @@
 import { ButtonProps } from './Button-props';
+import cn from 'classnames'
 import styles from './Button.module.css'
 
 export function Button({apperance, children}:ButtonProps) {
 	return (
-		<button />
+		<button
+			className={cn(styles.button, {
+				[styles.primary]: apperance === 'primary',
+				[styles.ghost]: apperance === 'ghost'
+			})}
+			>
+			{children}
+		</button>
 	)
 }
