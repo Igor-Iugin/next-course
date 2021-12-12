94377e4d0c608a7123d7da2a4428a729bb065d2c
Igor Iugin <animeneanime@mail.ru>
2021-12-12 16:59:46 +0300

    Added insert className from props

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,10 +2,10 @@ import { ButtonProps } from './Button-props';
 import cn from 'classnames'
 import styles from './Button.module.css'
 
-export function Button({apperance, children}:ButtonProps) {
+export function Button({apperance, className, children}:ButtonProps) {
 	return (
 		<button
-			className={cn(styles.button, {
+			className={cn(styles.button, className, {
 				[styles.primary]: apperance === 'primary',
 				[styles.ghost]: apperance === 'ghost'
 			})}
