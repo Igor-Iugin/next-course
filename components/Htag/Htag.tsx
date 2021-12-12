a8587c80b79217609bc512bcf6ea96ae6e713c16
Igor Iugin <animeneanime@mail.ru>
2021-12-12 15:35:04 +0300

    Add styles to component

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -1,9 +1,19 @@
 import { HtagProps } from './Htag-props'
+import styles from './Htag.module.css'
 
 export function Htag ({tag, children}:HtagProps):JSX.Element {
-	return (
-		<h1>
-			{children}
-		</h1>
-	)
+	switch (tag) {
+		case 'h1':
+			return <h1 className={styles.h1} >{children}</h1>
+
+		case 'h2':
+			return <h2 className={styles.h2} >{children}</h2>
+			
+		case 'h3':
+			return <h3 className={styles.h3} >{children}</h3>
+	
+		default:
+			return <>Invalid tag</>
+	}
+
 }
