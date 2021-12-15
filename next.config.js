10b1a0c599bb267a8922aabdd0b95997cd80ec48
Igor Iugin <animeneanime@mail.ru>
2021-12-15 23:58:41 +0300

    Installed & configured SVGR for including svg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+		domains: ['courses-top.ru']
+	},
+  webpack(config) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      options: {
+				prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{
+            name: 'preset-default',
+            params: {overrides: { removeViewBox: false }}
+          }]
+        },
+				titleProp: true,
+			},
+    })
+
+    return config
+  }
 }
