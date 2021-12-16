d378073a5bba53cba27aaa10fe5103e10d369d72
Igor Iugin <animeneanime@mail.ru>
2021-12-16 08:53:50 +0300

    Added arrow-icon & styles checking in button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,17 +1,23 @@
 import { ButtonProps } from './Button-props';
+import ArrowIcon from '../../public/images/arrow.svg'
+
 import cn from 'classnames'
+
 import styles from './Button.module.css'
 
-export function Button({apperance, className, children, ...props}:ButtonProps) {
+
+export function Button({apperance, className, arrow = null, children, ...props}:ButtonProps) {
 	return (
 		<button
 			className={cn(styles.button, className, {
 				[styles.primary]: apperance === 'primary',
-				[styles.ghost]: apperance === 'ghost'
+				[styles.ghost]: apperance === 'ghost',
+				[styles.hasArrow]: arrow
 			})}
 			{...props}
 			>
 			{children}
+			{arrow && <ArrowIcon />}
 		</button>
 	)
 }
