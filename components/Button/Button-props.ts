2fef12830a3361ddbd2307079dc01b495d4180c3
Igor Iugin <animeneanime@mail.ru>
2021-12-16 08:52:30 +0300

    Added arrow to props

--- components/Button/Button-props.ts
+++ components/Button/Button-props.ts
@@ -2,5 +2,6 @@ import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 
 export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
 	children: ReactNode,
-	apperance: 'primary' | 'ghost'
+	apperance: 'primary' | 'ghost',
+	arrow?: true | null;
 }
