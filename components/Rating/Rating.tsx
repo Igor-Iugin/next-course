1825998d17be14c291b22fccc668913056d8d14d
Igor Iugin <gigorsaurus@gmail.com>
2022-01-16 19:39:26 +0300

    Добавил эффект наведения на звёзды

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { Fragment, KeyboardEvent, useEffect, useState } from 'react';
 import { RatingProps } from './Rating-props';
 import StarIcon from './star.svg'
 
@@ -9,18 +9,44 @@ import styles from './Rating.module.css'
 
 export function Rating({isEditable, rating, setRating, children, className, ...props}:RatingProps) {
 	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>))
+	const [hoverRating, setHoverRating] = useState<number>(0)
 
 	useEffect(() => {
-		constractRating(rating)
-	}, [rating])
+		constractRating(hoverRating > 0 ? hoverRating : rating)
+	}, [rating, hoverRating])
+
+
+	const onStartHover = (ratingTemp:number) => {
+		if (!isEditable) {
+			return
+		}
+		setHoverRating(ratingTemp)
+	}
+
+	const onClick = (rate:number, event?:KeyboardEvent<SVGElement>) => {
+		if (!isEditable || !setRating) {
+			return
+		}
+
+		if (event) {
+			if (event.code !== 'Space') return
+		}
+
+		setRating(rate)
+	}
 
 	const constractRating = (currentRating: number) => {
-		const updatedArray = ratingArray.map((rating: JSX.Element, index: number) => {
+		const updatedArray = ratingArray.map((r: JSX.Element, index: number) => {
 			return (
 				<StarIcon 
 					className={cn(styles.star, {
-						[styles.filled]: index < currentRating
+						[styles.filled]: index < currentRating,
+						[styles.editable]: isEditable
 					})}
+					onMouseEnter={() => onStartHover(index + 1)}
+					onClick={() => onClick(index + 1)}
+					tabIndex={isEditable ? 0 : -1}
+					onKeyDown={(e:KeyboardEvent<SVGElement>) => onClick(index + 1, e)}
 				/>
 			)
 		})
@@ -29,8 +55,12 @@ export function Rating({isEditable, rating, setRating, children, className, ...p
 	}
 
 	return (
-		<div className={styles.stars} {...props}>
-			{ratingArray.map((rating, index) => <div key={index}>{rating}</div>)}
+		<div 
+			className={styles.stars}
+			onMouseLeave={() => onStartHover(0)}
+			{...props}
+			>
+			{ratingArray.map((rating, index) => <Fragment key={index}>{rating}</Fragment>)}
 		</div>
 	)
 }
