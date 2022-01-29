3b92e6959355bcd240f6e1ae07da70105f6d1e7b
Igor Iugin <gigorsaurus@gmail.com>
2022-01-29 19:09:20 +0300

    Made footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,16 @@
-import { FooterProps } from './Footer-props';
+import { FooterProps } from './Footer-props'
+import cn from 'classnames'
+import { format } from 'date-fns'
 
 import styles from './Footer.module.css'
 
 
-export function Footer({...props}:FooterProps) {
+export function Footer({ className, ...props }:FooterProps) {
 	return (
-		<footer {...props}>
+		<footer className={cn(styles.footer, className)} {...props}>
+			<div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+			<a href='#' target='_blank' rel='noopener'>Пользовательское соглашение</a>
+			<a href='#' target='_blank' rel='noopener'>Политика конфиденциальности</a>
 		</footer>
 	)
 }
