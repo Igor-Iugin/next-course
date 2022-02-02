8206dbaf8e5ebb3973292a8e04d3d87a67c6e812
Igor Iugin <gigorsaurus@gmail.com>
2022-02-02 21:56:36 +0300

    Used getStaticProps & added getting menu from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,38 @@
-import type { NextPage } from 'next'
-import { Rating } from '../components'
+import { GetStaticProps } from 'next'
+import { useState } from 'react'
+import axios, { AxiosResponse } from 'axios'
+
+import { withLayout } from '../layout/Layout'
+
+import { MenuItem } from '../interfaces/menuInterface'
 
 
-const Home: NextPage = () => {
+function Home({ menu, firstCategory }: HomeProps):JSX.Element {
   return (
-    <>
-      <Rating rating={4} />
-    </>
+    <ul>
+      {menu.map(elem => <li key={elem._id.secondCategory}>{elem._id.secondCategory}</li>)}
+    </ul>
   )
 }
 
-export default Home
+export default withLayout(Home)
+
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  })
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[],
+  firstCategory: number
+}
