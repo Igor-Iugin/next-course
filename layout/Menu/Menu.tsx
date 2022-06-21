93f56f7444bf81b0a5f05288194d5a96bd603118
Igor Iugin <gigorsaurus@gmail.com>
2022-06-21 21:55:45 +0300

    Начал разработку menu

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,22 +1,32 @@
-import cn from 'classnames'
 import { useContext } from 'react'
 import { AppContext } from '../../contexts/app.context'
 
-import styles from './Menu.module.css'
+import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface'
+import { TopLevelCategory } from '../../interfaces/page.interface'
 
+import CoursesIcon from './icons/cources.svg'
+import ServicesIcon from './icons/services.svg'
+import BooksIcon from './icons/books.svg'
+import ProductsIcon from './icons/products.svg'
+
+import styles from './Menu.module.css'
+import classNames from 'classnames/bind'
+
+
+const cn = classNames.bind(styles)
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+	{ route: 'products', name: 'Товары', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+]
 
 export function Menu() {
 	const { menu, setMenu, firstCategory } = useContext(AppContext)
 
-	const MenuList = (
-		<ul>
-			{menu.map(item => <li key={item._id.secondCategory}>{item._id.secondCategory}</li>)}
-		</ul>
-	)
-
 	return (
-		<nav>
-			{MenuList}
+		<nav className={styles.menu}>
 		</nav>
 	)
 }
