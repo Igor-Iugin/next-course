829d71f181bb9e9794c5a2159510cec03e0dfbff
Igor Iugin <gigorsaurus@gmail.com>
2022-06-26 16:05:10 +0300

    Сделал построение меню

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -25,8 +25,52 @@ const firstLevelMenu: FirstLevelMenuItem[] = [
 export function Menu() {
 	const { menu, setMenu, firstCategory } = useContext(AppContext)
 
+	const buildFirstLevel = () => {
+		return (
+			<ul>
+				{firstLevelMenu.map(menuList => (
+					<li key={menuList.route}>
+						<a className={cn('firstLevel', { active: menuList.id === firstCategory })} href={`/${menuList.route}`}>
+							{menuList.icon}
+							{menuList.name}
+						</a>
+						{menuList.id === firstCategory && buildSecondLevel(menuList)}
+					</li>
+				))}
+			</ul>
+		)
+	}
+
+	const buildSecondLevel = (menuParent: FirstLevelMenuItem) => {
+		return (
+			<ul>
+				{menu.map(menu => (
+					<li className={cn('secondLevel', { active: menu.isOpened })} key={menu._id.secondCategory}>
+						{menu._id.secondCategory}
+						{buildThirdLevel(menu.pages, menuParent.route)}
+					</li>
+				))}
+			</ul>
+		)
+	}
+
+	const buildThirdLevel = (pages: PageItem[], route: string) => {
+		return (
+			<ul>
+				{pages.map(page => (
+					<li key={page._id}>
+						<a href={`/${route}/${page.alias}`} className={cn('thirdLevel', { active: true })}>
+							{page.category}
+						</a>
+					</li>
+				))}
+			</ul>
+		)
+	}
+
 	return (
 		<nav className={styles.menu}>
+			{buildFirstLevel()}
 		</nav>
 	)
 }
